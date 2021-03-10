3d09082f03bcf69f9ba0cf506ad4efebd1ae4397
zaykho <zaykho@msn.com>
2021-03-10 02:47:55 +0100

    Update gamepad.js

--- docs/psx-core/gamepad.js
+++ docs/psx-core/gamepad.js
@@ -25,10 +25,10 @@ const handleGamePads = (() => {
       const device = joy.devices[0];
 
       device.lo = 0xff;
-      if (pad.axes[0] === -1) { device.lo &= ~0x80 } // left
-      if (pad.axes[0] ===  1) { device.lo &= ~0x20 } // right
-      if (pad.axes[1] === -1) { device.lo &= ~0x10 } // up
-      if (pad.axes[1] ===  1) { device.lo &= ~0x40 } // down
+      if (pad.axes[0] <= -0.4) { device.lo &= ~0x80 } // left
+      if (pad.axes[0] >=  0.4) { device.lo &= ~0x20 } // right
+      if (pad.axes[1] <= -0.4) { device.lo &= ~0x10 } // up
+      if (pad.axes[1] >=  0.4) { device.lo &= ~0x40 } // down
 
       if (pad.buttons[14].pressed) { device.lo &= ~0x80 } // left
       if (pad.buttons[15].pressed) { device.lo &= ~0x20 } // right
@@ -44,10 +44,10 @@ const handleGamePads = (() => {
       if (pad.buttons[0].pressed) { device.hi &= ~0x40 } // cross
       if (pad.buttons[2].pressed) { device.hi &= ~0x80 } // square
 
-      if (pad.buttons[6].pressed) { device.hi &= ~0x04 } // l1
-      if (pad.buttons[7].pressed) { device.hi &= ~0x01 } // l2
+      if (pad.buttons[4].pressed) { device.hi &= ~0x04 } // l1
+      if (pad.buttons[6].pressed) { device.hi &= ~0x01 } // l2
       if (pad.buttons[5].pressed) { device.hi &= ~0x08 } // r1
-      if (pad.buttons[4].pressed) { device.hi &= ~0x02 } // r2
+      if (pad.buttons[7].pressed) { device.hi &= ~0x02 } // r2
     }
   }
 
