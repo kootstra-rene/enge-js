e15514d6d5ee49f889ef2d682cb026326b66405e
Rene Kootstra <rene.kootstra@signify.com>
2021-09-29 23:22:12 +0200

    #16 extracting utils module

---
+++ docs/utils.js
@@ -0,0 +1,22 @@
+((scope) => {
+
+  function readStorageStream(item, handler) {
+    const base64text = localStorage.getItem(item);
+    if (base64text) {
+      const arrayBuffer = Base64.decode(base64text);
+      return handler(arrayBuffer);
+    }
+    else {
+      return handler(null);
+    }
+  }
+  
+  function writeStorageStream(item, arrayBuffer) {
+    const base64text = Base64.encode(arrayBuffer);
+    localStorage.setItem(item, base64text);
+  }
+
+  scope.readStorageStream = readStorageStream;
+  scope.writeStorageStream = writeStorageStream;
+  
+})(window);
