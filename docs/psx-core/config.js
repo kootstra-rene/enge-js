fc78f940045d03f344ad5e2fd816b92c89aa29f1
Rene Kootstra <rene.kootstra@signify.com>
2021-10-11 22:34:26 +0200

    properly hiding more implementation details

--- docs/psx-core/config.js
+++ docs/psx-core/config.js
@@ -1,22 +1,28 @@
-'use strict';
+(scope => {
 
-const settings = (() => {
-	let object = JSON.parse(localStorage.getItem('config') || '{"quality": 1}');
-	if (object.naiveResolutionImprovement === undefined) object.naiveResolutionImprovement = true;
-	return object;
-})();
+	'use strict';
+
+	const settings = (() => {
+		let object = JSON.parse(localStorage.getItem('config') || '{"quality": 1}');
+		if (object.naiveResolutionImprovement === undefined) object.naiveResolutionImprovement = true;
+		return object;
+	})();
 
 
-settings.updateQuality = update => {
-	const elem = document.getElementById('quality');
+	settings.updateQuality = update => {
+		const elem = document.getElementById('quality');
 
-	if (update) {
-		settings.quality <<= 1;
-		if (settings.quality > 8) settings.quality = 1;
+		if (update) {
+			settings.quality <<= 1;
+			if (settings.quality > 8) settings.quality = 1;
 
-		localStorage.setItem('config', JSON.stringify(settings));
-		elem.classList.add('restart');
+			localStorage.setItem('config', JSON.stringify(settings));
+			elem.classList.add('restart');
+		}
+
+		elem.innerText = `Q${settings.quality}`;
 	}
 
-	elem.innerText = `Q${settings.quality}`;
-}
+	scope.settings = settings;
+
+})(window);
